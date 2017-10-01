3cbcad80ba8736c75bac0174cc43a8083afb4e44
ljan <schick.ne.email@gmail.com>
2017-10-01 11:20:34 +0200

    Added WifiManager
    Coppied WiFiManager Example with additional Parameters and custom static IP
    https://github.com/tzapu/WiFiManager/tree/master/examples/AutoConnectWithFSParametersAndCustomIP

--- ESP_mqtt-temphum.ino
+++ ESP_mqtt-temphum.ino
@@ -1,20 +1,47 @@
-#include <ESP8266WiFi.h>
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
 
-#include <PubSubClient.h>
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 
-#include "DHT.h"
+// needed for WifiManager
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+#include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
 
-#include "config.h"
-#include "debug.h"
+// needed for Functionality
+#include <PubSubClient.h>         // MQTT Client
+#include "DHT.h"                  // Temperature Sensor
+#include "config.h"               // Config
+#include "debug.h"                // debugging
 
-const int AttemptDelay = 10;        // Delay in ms between measurement attempts
+// Config
+const int AttemptDelay = 10;      // Delay in ms between measurement attempts
+
+// Data to go into FS
+// define your default values here, if there are different values in config.json, they are overwritten.
+// length should be max size + 1 
+char mqtt_server[40] = MQTT_SERVER;
+char mqtt_port[6] = MQTT_PORT;
+// default custom static IP
+char static_ip[16] = DEFAULT_IP;
+char static_gw[16] = DEFAULT_QG;
+char static_sn[16] = DEFAULT_SN;
+
+
+bool shouldSaveConfig = false;    // flag for saving data
+
+// callback notifying us of the need to save config
+void saveConfigCallback () {
+  dbprintln("Should save config");
+  shouldSaveConfig = true;
+}
 
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
 
 DHT dht(DHT_PIN, DHT_TYPE);
 
-ADC_MODE(ADC_VCC);  // Read internal vcc rather than voltage on ADC pin (A0 must be floating)
+ADC_MODE(ADC_VCC);  // Read internal VCC rather than voltage on ADC pin (A0 must be floating)
 
 // *******SETUP*******
 /* Bootup, Power and Initialize DHT, Setup Wifi and MQTT */
@@ -34,11 +61,140 @@ void setup() {
   dht.readTemperature();  // first reading to initialize DHT
   dht.readHumidity();
   
-  // start wifi
-  setup_wifi();
+  //clean FS, for testing
+  //SPIFFS.format();
+
+  //read configuration from FS json
+  dbprintln("mounting FS...");
+
+  if (SPIFFS.begin()) {
+    dbprintln("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      dbprintln("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        dbprintln("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          dbprintln("\nparsed json");
+
+          strcpy(mqtt_server, json["mqtt_server"]);
+          strcpy(mqtt_port, json["mqtt_port"]);
+
+          if(json["ip"]) {
+            dbprintln("setting custom ip from config");
+            //static_ip = json["ip"];
+            strcpy(static_ip, json["ip"]);
+            strcpy(static_gw, json["gateway"]);
+            strcpy(static_sn, json["subnet"]);
+            //strcat(static_ip, json["ip"]);
+            //static_gw = json["gateway"];
+            //static_sn = json["subnet"];
+            dbprintln(static_ip);
+/*            dbprintln("converting ip");
+            IPAddress ip = ipFromCharArray(static_ip);
+            dbprintln(ip);*/
+          } else {
+            dbprintln("no custom ip in config");
+          }
+        } else {
+          dbprintln("failed to load json config");
+        }
+      }
+    }
+  } else {
+    dbprintln("failed to mount FS");
+  }
+  //end read
+  
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
+  WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 5);
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  //set static ip
+  IPAddress _ip,_gw,_sn;
+  _ip.fromString(static_ip);
+  _gw.fromString(static_gw);
+  _sn.fromString(static_sn);
+
+  wifiManager.setSTAStaticIPConfig(_ip, _gw, _sn);
+  
+  //add all your parameters here
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.addParameter(&custom_mqtt_port);
+
+  //reset settings - for testing
+  //wifiManager.resetSettings();
+
+  //set minimu quality of signal so it ignores AP's under that quality
+  //defaults to 8%
+  wifiManager.setMinimumSignalQuality();
+  
+  //sets timeout until configuration portal gets turned off
+  //useful to make it all retry or go to sleep
+  //in seconds
+  wifiManager.setTimeout(180);
+
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect()) {
+    dbprintln("failed to connect and hit timeout");
+    delay(3000);
+    //reset and try again, or maybe put it to deep sleep
+    gotodeepsleep(SLEEP_TIME_S);
+  }
+  //if you get here you have connected to the WiFi
+  dbprintln("connected...yeey :)");
+  
+  //read updated parameters
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(mqtt_port, custom_mqtt_port.getValue());
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    dbprintln("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["mqtt_server"] = mqtt_server;
+    json["mqtt_port"] = mqtt_port;
+
+    json["ip"] = WiFi.localIP().toString();
+    json["gateway"] = WiFi.gatewayIP().toString();
+    json["subnet"] = WiFi.subnetMask().toString();
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      dbprintln("failed to open config file for writing");
+    }
+
+    json.prettyPrintTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+  }
+  dbprintln();
   
   // setup mqtt
-  mqttClient.setServer(MQTT_SERVER, 1883);
+  mqttClient.setServer(mqtt_server, atoi(mqtt_port)); // parseInt to the port
   reconnect_mqtt();
   mqttClient.loop(); // This allows the client to maintain the connection and check for any incoming messages.
   yield();
@@ -139,7 +295,7 @@ void reconnect_mqtt() {
       // Wait 1 second before retrying
       delay(1000);
       if(fails > 3) {
-        gotodeepsleep(sleepTimeS);
+        gotodeepsleep(SLEEP_TIME_S);
       }
       fails++;
     }
