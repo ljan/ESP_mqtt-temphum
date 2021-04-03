6fa0f50bb8e61eed402d350f06cabc842ce60c0d
Jan <schick.ne.email@gmail.com>
2021-04-03 19:23:55 +0200

    Update debug.h
    included wait for serial

--- src/debug.h
+++ src/debug.h
@@ -1,7 +1,7 @@
 #ifdef DEBUG
  #define dbprint(x)  Serial.print(x)
  #define dbprintln(x)  Serial.println(x)
- #define dbserialbegin(x) Serial.begin(x);
+ #define dbserialbegin(x) Serial.begin(x); while (!Serial) continue;
 #else
  #define dbprint(x)
  #define dbprintln(x)
